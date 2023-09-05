75b28417ca338bae087eea5160d111b25c9e2dd2
daniil <daniilzhilyaev@gmail.com>
2023-09-05 15:54:23 +0300

    task 2

---
+++ 003_Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿/**Задача 2:** Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+M = 1; N = 5. -> "1, 2, 3, 4, 5"
+M = 4; N = 8. -> "4, 5, 6, 7, 8"*/
+
+Console.WriteLine("Введите число M: ");
+int m = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"{NatNumbers(m, n)}");
+
+string NatNumbers(int numberstart, int numberfinish)
+{
+    if (numberstart <= numberfinish)
+    {
+        if (numberstart == numberfinish) return numberstart.ToString();
+        return (((numberstart) + ", " + NatNumbers(numberstart + 1, numberfinish)));
+    }
+    else return ("Число M меньше числа N");
+}
