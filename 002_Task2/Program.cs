6f69f174cfd32c07db0748e3b3a10c265cc6b506
daniil <daniilzhilyaev@gmail.com>
2023-09-05 14:48:37 +0300

    task 1

---
+++ 002_Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 1:** Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+
+N = 5 -> "1, 2, 3, 4, 5"
+
+N = 6 -> "1, 2, 3, 4, 5, 6" */
+
+Console.WriteLine("Введите число N: ");
+int number = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"{NatNumbers(number, 1)}");
+
+string NatNumbers(int numbers, int i)
+{
+    if (numbers == i) return i.ToString();
+    return (((i)  + ", " + NatNumbers(numbers, i +1)));
+}
