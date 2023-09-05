b52e36f1bb6a023cc300b4f0b8977259a67642d2
daniil <daniilzhilyaev@gmail.com>
2023-09-05 13:37:03 +0300

    Example task

--- 001_Task1/Program.cs
+++ 001_Task1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая будет принимать на вход
+//число и возвращать сумму его цифр.
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Сумма цифр числа {number} равна {SumNumbers(number)}");
+
+int SumNumbers(int numbers)
+{
+    if (numbers ==0) return 0;
+    return (numbers % 10 + SumNumbers(numbers / 10));
+}
